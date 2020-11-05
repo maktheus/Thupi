3116eb07b5b5b7a510842b3a420e5c1fe4116ab0
Matheus <maktheus@gmail.com>
2020-11-05 12:37:05 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express =require('express');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors')
 
 
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.use(cors());
+app.listen(process.env.PORT || 3000);
